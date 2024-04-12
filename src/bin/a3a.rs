b964675f2d06cfbe9d903b357b62622667826d8d
hy43 <hyde0401@gmail.com>
2024-04-12 16:28:05 +0900

    feat : solve control flow (if & else)

--- src/bin/a3a.rs
+++ src/bin/a3a.rs
@@ -10,4 +10,12 @@
 // * Use an if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    let my_bool = true;
+
+    if my_bool {
+        println!("hello");
+    } else {
+        println!("goodbye");
+    }
+}
