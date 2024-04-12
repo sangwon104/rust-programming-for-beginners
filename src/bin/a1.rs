5f02225c831e09218c3b216568186a498b28570a
hy43 <hyde0401@gmail.com>
2024-04-12 15:00:06 +0900

    feat : solve function practice

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -8,4 +8,15 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    print_my_first_name();
+    print_my_last_name();
+}
+
+fn print_my_first_name() {
+    println!("sangwon");
+}
+
+fn print_my_last_name() {
+    println!("cho");
+}
