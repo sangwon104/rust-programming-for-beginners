e3b45f031a36453fa11d7eee269e3fead7ffdddb
hy43 <hyde0401@gmail.com>
2024-04-13 20:36:14 +0900

    feat : solve enum

--- src/bin/a7.rs
+++ src/bin/a7.rs
@@ -10,4 +10,22 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+enum Color {
+    Red,
+    Blue,
+    Black,
+}
+
+fn print_color (color: Color) {
+    match color {
+        Color::Red => println!("red!"),
+        Color::Blue => println!("blue!"),
+        Color::Black => println!("black!")
+    }
+}
+
+fn main() {
+    print_color(Color::Red);
+    print_color(Color::Black);
+    print_color(Color::Blue);
+}
