90d2af5c4aab9bd474a3c2c4caa9edbf01204695
hy43 <hyde0401@gmail.com>
2024-04-13 20:53:59 +0900

    feat : solve structure

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -9,4 +9,30 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum Flavor {
+    Orange,
+    Apple,
+    Banana,
+}
+
+struct Drink {
+    flavor: Flavor,
+    fluid_ounce: f64,
+}
+
+fn print_drink_info (drink: Drink) {
+    println!("fluid ounce : {:?}", drink.fluid_ounce);
+    match drink.flavor {
+        Flavor::Orange => println!("flavor : orange!"),
+        Flavor::Apple => println!("flavor : apple!"),
+        Flavor::Banana => println!("flavor : banana")
+    }
+}
+
+fn main() {
+    let drink: Drink = Drink {
+        flavor: Flavor::Orange,
+        fluid_ounce: 2.99
+    };
+    print_drink_info(drink);
+}
